59f7a000e818d1bc6a50c1b447040a63da21116f
Petr Majer <majer@domena.demlova.ji.cz>
2017-12-19 10:48:12 +0100

    JS - uprava funkce Addcardtolist, porovnani karet vytazene do samostatne fce

--- js/app.js
+++ js/app.js
@@ -54,17 +54,6 @@ function buildGame() {
 	}
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
 function cardOpen(target) {
 	$(target).removeClass('close');
 	$(target).addClass('show open');
@@ -92,17 +81,21 @@ function resetList(){
 
 function addCardToList(target) {
 	let card = $(target).children().attr('class'); //zacileni na ikonu ktera je umistena v targetu - li;
-	
+
 
 	if (cardList.length < 2) {
 		cardList.push(card);
 		targetList.push(target);  //pole pro ukladani jquerry cilu - karet
 	}
 
-	if (cardList.length === 2){
-		cardList[0] === cardList[1] ? cardMatch(targetList) : setTimeout('cardClose(targetList)',500);
-		counter();
+	if (cardList.length === 2) {
+		compareCards();
 	}
+}
+
+function compareCards(){
+	cardList[0] === cardList[1] ? cardMatch(targetList) : setTimeout('cardClose(targetList)',500);
+	counter();
 }
 
 function counter () {
@@ -128,3 +121,14 @@ $('ul').on('click','li', function (evt) {
 	}
 
 });
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
