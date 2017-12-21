d3eb16067d14b948bb97f9f77979470d0c08430a
PeMajer <majer.petr@gmail.com>
2017-12-21 21:44:29 +0100

    JS - prehozeni fci leaderboardu do app.js

--- js/app.js
+++ js/app.js
@@ -167,6 +167,33 @@ function displayStars() {
 	}
 }
 
+function leaderBoard() {
+	$('.leaderboard').remove();
+	const data = localStorage.getItem('results');
+	if (data) {
+  		const results = JSON.parse(data);
+
+  		$('<div class="leaderboard"><h1>LeaderBoard</h1><div>').insertAfter('.deck');
+
+  		for (const res of results ) {
+	  		$('.leaderboard').append('<h2> Moves:' + res.move + ' ' + res.star + '  ' + res.time + '</h2>');
+	  	}
+	}
+}
+
+function addResult(result){
+	const data = localStorage.getItem('results');
+	if (data) {
+	  		let results = JSON.parse(data);
+	  		results.push(result);
+	  		localStorage.setItem('results', JSON.stringify(results));
+	  	} else {
+	  		let results = [];
+	  		results.push(result);
+	  		localStorage.setItem('results', JSON.stringify(results));
+	  	}
+}
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
--- js/board.js
+++ js/board.js
@@ -1,32 +1,4 @@
-function leaderBoard() {
-	$('.leaderboard').remove();
-	const data = localStorage.getItem('results');
-	if (data) {
-  		const results = JSON.parse(data);
 
-  		$('<div class="leaderboard"><h1>LeaderBoard</h1><div>').insertAfter('.deck');
-
-  		for (const res of results ) {
-	  		$('.leaderboard').append('<h2> Moves:' + res.move + ' ' + res.star + '  ' + res.time + '</h2>');
-	  	}
-  		console.log('data v localu');
-  		console.log(results);
-	}
-	console.log('leaderboard');
-}
-
-function addResult(result){
-	const data = localStorage.getItem('results');
-	if (data) {
-	  		let results = JSON.parse(data);
-	  		results.push(result);
-	  		localStorage.setItem('results', JSON.stringify(results));
-	  	} else {
-	  		let results = [];
-	  		results.push(result);
-	  		localStorage.setItem('results', JSON.stringify(results));
-	  	}
-}
 
 
 function clearResults() {
